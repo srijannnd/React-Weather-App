18ce82d9bc93d4b35d8c1bb1f5d8760e493b28a3
srijan <srijannnd@gmail.com>
2018-02-19 17:46:03 +0530

    https-issue-resolved

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '5c4542b4abeb247906bd378d76393670';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
